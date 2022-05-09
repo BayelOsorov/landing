dc47f05418ea7d3c9bcb45f89764a08fda48b70d
BayelOsorov <osorov.bayel@gmail.com>
2022-05-09 22:01:44 +0600

    fix bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,17 @@
-import React, { lazy, Suspense, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import NavBar from "../components/NavBar/NavBar";
 import Header from "../components/Header/Header";
-// ?replace to lazy loading
-// import About from "../components/About/About";
-// import HowToUse from "../components/HowToUse/HowToUse";
-// import Benefits from "../components/Benefits/Benefits";
-// import Questions from "../components/Questions/Questions";
-// import Refill from "../components/Refill/Refill";
-// import Footer from "../components/Footer/Footer";
+import About from "../components/About/About";
+import HowToUse from "../components/HowToUse/HowToUse";
+import Benefits from "../components/Benefits/Benefits";
+import Questions from "../components/Questions/Questions";
+import Refill from "../components/Refill/Refill";
+import Footer from "../components/Footer/Footer";
 
 import AOS from "aos";
 import "swiper/css";
 import "aos/dist/aos.css";
-const About = lazy(() => import("../components/About/About"));
-const HowToUse = lazy(() => import("../components/HowToUse/HowToUse"));
-const Benefits = lazy(() => import("../components/Benefits/Benefits"));
-const Footer = lazy(() => import("../components/Footer/Footer"));
-const Refill = lazy(() => import("../components/Refill/Refill"));
-const Questions = lazy(() => import("../components/Questions/Questions"));
 
 const Home = () => {
   useEffect(() => {
@@ -29,13 +22,11 @@ const Home = () => {
       <NavBar />
       <Header />
       <About />
-      <Suspense fallback={<i>Loading...</i>}>
-        <HowToUse />
-        <Benefits />
-        <Questions />
-        <Refill />
-        <Footer />
-      </Suspense>
+      <HowToUse />
+      <Benefits />
+      <Questions />
+      <Refill />
+      <Footer />
     </>
   );
 };
